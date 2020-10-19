9354ee20314e3b756f1551486c5525442faf2002
wuchienchou <johnson12440@gmail.com>
2020-10-19 01:04:40 -0500

    fix bug ok, try to push the rest again

--- home/forms.py
+++ home/forms.py
@@ -1,12 +1,12 @@
 from django import forms
 from .models import Post#, Category
 
-#choices = Category.objects.all().values_list('name', 'name')
+choices = Category.objects.all().values_list('name', 'name')
 
-#choice_list = []
+choice_list = []
 
-#for item in choices:
-#	choice_list.append(item)
+for item in choices:
+	choice_list.append(item)
 
 class PostForm(forms.ModelForm):
 	class Meta:
@@ -16,6 +16,6 @@ class PostForm(forms.ModelForm):
 			'title': forms.TextInput(attrs={'class': 'form-control'}),
 			'title_tag': forms.TextInput(attrs={'class': 'form-control'}),
 			'author': forms.Select(attrs={'class': 'form-control'}),
-#			'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
+			'category': forms.Select(choices=choice_list, attrs={'class': 'form-control'}),
 			'body': forms.Textarea(attrs={'class': 'form-control'}),
 		}
