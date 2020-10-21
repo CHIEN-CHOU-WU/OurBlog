20a7f7ee3d22702c6efc6ef29af645bfef86be12
wuchienchou <johnson12440@gmail.com>
2020-10-21 00:31:00 -0500

    migrate fb ins linkedin website to profile

--- home/models.py
+++ home/models.py
@@ -55,8 +55,13 @@ class Category(models.Model):
 
 
 class Profile(models.Model):
-	user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
-	bio = models.TextField()
+	user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)      # 把 profile model 和 user 連結在一起 
+	bio = models.TextField()											        # 自介
+	profile_pic = models.ImageField(null=True, blank=True, upload_to="image/profile/")
+	website_url = models.CharField(max_length=255, null=True, blank=True)
+	instagram_url = models.CharField(max_length=255, null=True, blank=True)
+	facebook_url = models.CharField(max_length=255, null=True, blank=True)
+	linkedin_url = models.CharField(max_length=255, null=True, blank=True)
 
 	def __str__(self):
 		return str(self.user)
